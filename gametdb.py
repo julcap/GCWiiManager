86c681a5d7a02d8f1e5ce9f6e11b6fc999af518b
Julian C <lyhan_jr@hotmail.com>
2016-05-01 20:51:50 +0200

    Updates

--- gametdb.py
+++ gametdb.py
@@ -1,6 +1,6 @@
 import urllib.request
 import urllib.parse
-import os
+import os,re
 
 BASE_URL = 'http://www.gametdb.com'
 DISC_PATH =  os.path.join(os.getcwd(),'wii','disc')
@@ -9,7 +9,7 @@ COVER3D_PATH = os.path.join(os.getcwd(),'wii','cover3D')
 class GameTDB():
     def __init__(self):
         self.gameList = 'wiitdb.txt'
-
+        self.downloads = 'Wii/Downloads'
     def getGameList(self,language = 'EN'):
         gamesDict = dict()
         params =  { 'LANG' : language}
@@ -26,22 +26,31 @@ class GameTDB():
         if language == None:
             return 0
         else:
+            params = { 'LANG' : language }
+            data = urllib.parse.urlencode(params)
+            data = data.encode('utf-8')
+            webData = urllib.request.urlopen(BASE_URL + '/' + self.downloads,data)
             if disc:
-                self.getDiscArtWork(language)
-            elif cover3D:
-                self.getCover3DArtWork(language)
+                self.getDiscArtWork(language,self.getURL('disc',language,webData))
+            if cover3D:
+                self.getCover3DArtWork(language,self.getURL('cover3D',language,webData))
             else:
                 return 0
 
-    def getDiscArtWork(self,language):
-        pass
+    def getURL(self,item,language,webData):
+        for i in webData:
+            result = re.findall('.+GameTDB-wii_' + item + '-' + language ,i.decode('utf-8'))
+            for i in str(result).split('href=')[1:]:
+                match = re.findall('.+GameTDB-wii_' + item +'-' + language +'.+', str(i.split(' ')[:1]))
+                if match:
+                    return str(match).strip('[\'\\]\"')
 
-    def getCover3DArtWork(self,language):
-        pass
+    def getDiscArtWork(self,language,URL):
+        print(URL)
 
-
-
+    def getCover3DArtWork(self,language,URL):
+        print(URL)
 
 gametdb = GameTDB()
-gamelist = gametdb.getGameList()
-print(len(gamelist.items()))
+gamelist = gametdb.getArtWorks('EN',True,True)
+
