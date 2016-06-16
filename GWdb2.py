0b08186f64348a5f3484a922eb60bddf75751d69
Julian C <lyhan_jr@hotmail.com>
2016-06-17 00:01:55 +0200

    Updates

--- GWdb2.py
+++ GWdb2.py
@@ -7,24 +7,40 @@ class GWdb(object):
         self.con = sqlite3.connect(":memory:")
         self.cur = self.con.cursor()
         qry = "CREATE TABLE IF NOT EXISTS gameTitles('id' INTEGER PRIMARY KEY,'code' TEXT,'title' TEXT)"
-        self.con.execute(qry)
+        self.cur.execute(qry)
         qry = "CREATE TABLE IF NOT EXISTS gamesFound('id' INTEGER PRIMARY KEY,'code' TEXT,'path' TEXT, 'fileType' TEXT, 'listName' TEXT)"
-        self.con.execute(qry)
+        self.cur.execute(qry)
+        self.con.commit()
 
     # def delete():
     #     ''' Delete database file '''
     #     if os.path.exists(database):
     #         os.remove(database)
-
-    def select(self,*args):
+    def insert(self,tableName,columns,values):
         """
-        :param args: Table name followed by dictionary with arguments for the where clause
+        :param args:
+        :string table name
+        :tuple columns
+        :list of tuples values
         :return:
         """
-        self.tableName = args[0] or None
-        self.arguments = args[1] or None
-        if self.tableName == None:
-            return 0
+        self.tableName = tableName
+        self.columns = columns
+        self.values = values
+        qry = "insert into {}{} values{}".format(self.tableName,self.columns,str(self.values).strip('[]'))
+        print(qry)
+        self.cur.execute(qry)
+        self.con.commit()
+
+
+    def select(self,tableNAme,arguments = None):
+        """
+        :param tableNAme str
+        :param arguments dict
+        :return:
+        """
+        self.tableName = tableNAme
+        self.arguments = arguments
         qry = "select * from {}".format(self.tableName)
         if self.arguments:
             arg = " where "
@@ -34,7 +50,7 @@ class GWdb(object):
                 if  list(self.arguments.keys()).index(i) != count -1:
                     arg += " and "
             qry = qry + arg
-        self.con.execute(qry)
+        self.cur.execute(qry)
         print(self.cur.fetchall())
 
 
@@ -50,7 +66,10 @@ class GWdb(object):
 
 
 test = GWdb()
-arguments = { 'code' : 'abc'}#,
-              #'title' : 'test'}
-test.select('gameTitles',arguments)
+columns = '(code,title)'
+values = [('abc','El mundo'),('123','la vista')]
+test.insert('gameTitles',columns,values)
+arguments = { 'code' : 'abc',
+              'title' : 'test'}
+test.select('gameTitles')
 test.close()
